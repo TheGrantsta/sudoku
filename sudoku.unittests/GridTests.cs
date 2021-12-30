086e5e345a32669dc0362fd9ae28a5416c9b8fe0
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2021-12-30 16:22:08 +0000

    Add grid for holding rows

---
+++ sudoku.unittests/GridTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace sudoku.unittests
+{
+    public class GridTests
+	{
+		[Fact]
+		public void ShouldReturnEmptyRowsForGrid()
+        {
+			var grid = new Grid();
+
+			grid.Rows.Should().NotBeNullOrEmpty();
+			grid.Rows.Count.Should().Be(9);
+        }
+	}
+}
+
---
+++ sudoku/Grid.cs
@@ -0,0 +1,20 @@
+﻿namespace sudoku
+{
+    public class Grid
+	{
+		private readonly List<Row> _rows;
+
+		public IReadOnlyList<Row> Rows => _rows;
+
+		public Grid()
+		{
+			_rows = new List<Row>();
+
+            for (int r = 1; r < 10; r++)
+            {
+                _rows.Add(new Row(r));
+            }
+		}
+	}
+}
+
