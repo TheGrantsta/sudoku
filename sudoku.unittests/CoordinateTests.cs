b2682963658fcadd5aa081204c3751979d602785
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2021-12-30 13:15:29 +0000

    Add coordinate class

---
+++ sudoku.unittests/CoordinateTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace sudoku.unittests
+{
+    public class CoordinateTests
+    {
+        [Fact]
+        public void ShouldThrowArgumentExceptionWhenRowIsLessThanOne()
+        {
+            Action coord = () => new Coordinate(1, 0);
+
+            coord.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void ShouldThrowArgumentExceptionWhenColumnIsLessThanOne()
+        {
+            Action coord = () => new Coordinate(1, 0);
+
+            coord.Should().Throw<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void ShouldReturnConstructedProperties()
+        {
+            var coord = new Coordinate(2, 7);
+
+            coord.Row.Should().Be(2);
+            coord.Column.Should().Be(7);
+        }
+
+        [Theory]
+        [InlineData(1, 6, "A6")]
+        [InlineData(2, 6, "B6")]
+        [InlineData(3, 6, "C6")]
+        [InlineData(4, 6, "D6")]
+        [InlineData(5, 6, "E6")]
+        [InlineData(6, 6, "F6")]
+        [InlineData(7, 6, "G6")]
+        [InlineData(8, 6, "H6")]
+        [InlineData(9, 6, "I6")]
+        public void ShouldReturnDisplayName(int row, int column, string expected)
+        {
+            var coord = new Coordinate(row, column);
+
+            coord.DisplayName.Should().Be(expected);
+        }
+    }
+}
+
---
+++ sudoku/Coordinate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace sudoku
+{
+	public class Coordinate
+	{
+		private readonly int _row;
+
+		private readonly int _column;
+
+		public int Row => _row;
+
+		public int Column => _column;
+
+		public string DisplayName => GetDisplayName();
+
+		public Coordinate(int row, int column)
+		{
+			if (IsOutOfRange(row))
+			{
+				throw new ArgumentOutOfRangeException($"Row parameter {row} out of range");
+			}
+
+			if (IsOutOfRange(column))
+			{
+				throw new ArgumentOutOfRangeException($"Column parameter {column} out of range");
+			}
+
+			_row = row;
+			_column = column;
+		}
+
+		private string GetDisplayName()
+        {
+            var rowLetters = new string[]{ "A", "B", "C","D", "E", "F","G", "H", "I"};
+
+			return $"{rowLetters[_row - 1]}{_column}";
+        }
+
+		private static bool IsOutOfRange(int i)
+		{
+			return i < 1 || i > 9;
+		}
+	}
+}
+
