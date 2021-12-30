945a529a66397dff356e769f706af63cc711b237
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2021-12-30 16:38:22 +0000

    Add simple console for rendering the grid

---
+++ sudoku.ui/Program.cs
@@ -0,0 +1,13 @@
+﻿using sudoku;
+using sudoku.ui;
+
+Console.WriteLine("Sudoku resolver!");
+Console.WriteLine("****************");
+Console.WriteLine("");
+
+var render = new Render(new Grid());
+
+render.Display();
+
+Console.WriteLine("");
+Console.WriteLine("****************");
---
+++ sudoku.ui/Render.cs
@@ -0,0 +1,25 @@
+﻿namespace sudoku.ui
+{
+    public class Render
+	{
+		private readonly Grid _grid;
+
+		public Render(Grid grid)
+		{
+			_grid = grid;
+		}
+
+		public void Display()
+        {
+            ((List<Row>)_grid.Rows).ForEach(s => RowOuput(s));
+        }
+
+		private static void RowOuput(Row r)
+        {
+			((List<Square>)r.Squares).ForEach(s => Console.Write("|_|"));
+			
+			Console.WriteLine("");
+        }
+	}
+}
+
