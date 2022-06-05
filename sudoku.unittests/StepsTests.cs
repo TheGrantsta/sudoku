351e6a6247093a921853aafb442dd1eef8adb21d
Grant McKenna <grant.mckenna@ofgem.gov.uk>
2022-06-05 08:14:15 +0100

    Amend Steps to return queue

---
+++ sudoku.unittests/StepsTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace sudoku.unittests
+{
+    public class StepsTests
+	{
+        [Fact]
+		public void ShouldAddStep()
+        {
+            var steps = new Steps();
+
+            steps.Add("A1", 1, false);
+
+            steps.Get().TryDequeue(out Step step);
+
+            step.Coordinate.Should().Be("A1");
+            step.Value.Should().Be(1);
+            step.IsGuess.Should().Be(false);
+        }
+
+        [Fact]
+        public void ShouldRemoveGuessSteps()
+        {
+            var steps = new Steps();
+
+            steps.Add("A1", 1, false);
+            steps.Add("A2", 1, false);
+            steps.Add("A3", 1, false);
+            steps.Add("A4", 1, true);
+
+            steps.RemoveGuesses();
+
+            for(var i = 0; i < 4; i++)
+            {
+                steps.Get().TryDequeue(out _);
+            }
+
+            steps.Get().IsEmpty.Should().BeTrue();
+        }
+	}
+}
+
---
+++ sudoku/Steps.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Concurrent;
+
+namespace sudoku
+{
+    public interface ISteps
+    {
+        void Add(string coordinate, int value, bool isGuess);
+
+        void RemoveGuesses();
+
+        ConcurrentQueue<Step> Get();
+    }
+
+    public class Steps : ISteps
+    {
+        private ConcurrentQueue<Step> _queue;
+
+        public Steps()
+        {
+            _queue = new ConcurrentQueue<Step>();
+        }
+
+        public void Add(string coordinate, int value, bool isGuess)
+        {
+            _queue.Enqueue(new Step { Coordinate = coordinate, Value = value, IsGuess = isGuess });
+        }
+
+        public void RemoveGuesses()
+        {
+            var tempQueue = new ConcurrentQueue<Step>();
+
+            foreach(var step in _queue)
+            {
+                if (!step.IsGuess)
+                {
+                    tempQueue.Enqueue(step);
+                }
+            }
+
+            _queue = tempQueue;
+        }
+
+        public ConcurrentQueue<Step> Get()
+        {
+            return _queue;
+        }
+    }
+
+    public struct Step
+    {
+        public string Coordinate { get; set; }
+
+        public int Value { get; set; }
+
+        public bool IsGuess { get; set; }
+    }
+}
+
